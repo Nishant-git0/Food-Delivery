c00e80db6825cb4e37c7d308d2486ad5a2647517
Nishant Thakur <139964109+Nishant-git0@users.noreply.github.com>
2025-07-10 08:25:49 +0545

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,11 +7,11 @@ import List from './pages/List/List';
 import Orders from './pages/Orders/Orders';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { API_BASE_URL } from './config.js';  // Add this import
+import { API_BASE_URL } from './config.js';
 
 const App = () => {
 
-  const url = API_BASE_URL;  // Change this line
+  const url = API_BASE_URL;
 
   return (
     <div>
@@ -21,6 +21,7 @@ const App = () => {
       <div className="app-content">
         <Sidebar/>
         <Routes>
+          <Route path='/' element={<Add url={url} />} />  {/* Add default route */}
           <Route path='/add' element={<Add url={url} />} />
           <Route path='/list' element={<List url={url}/>} />
           <Route path='/orders' element={<Orders url={url}/>} />
